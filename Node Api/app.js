6890f61cb42461195d5bf383670a0ff5139244a6
gaurav-celestial <gaurav.yadav@celestialsys.com>
2024-02-13 15:45:47 +0530

    upate

--- Node Api/app.js
+++ Node Api/app.js
@@ -69,6 +69,7 @@ app.get("/api/fetchRecipes", async (req, res) => {
       }
     }, 1000);
   } catch (err) {
+    res.status(400).json("not found");
     console.log(err);
   }
 });
@@ -76,12 +77,87 @@ app.get("/api/fetchRecipes", async (req, res) => {
 app.get("/api/fetchRecipes/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const recipe = await pool.query(
-      "SELECT * FROM recipe_summary WHERE id=$1",
-      [id]
-    );
+    const recipe = await pool.query("SELECT * FROM recipe WHERE id=$1", [id]);
     return res.status(200).json(recipe.rows);
   } catch (err) {
+    console.log(err);
+  }
+});
+
+app.put("/api/fetchRecipes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title } = req.body;
+    console.log(title, id);
+    await pool.query("UPDATE recipe SET title = $1 WHERE id = $2", [title, id]);
+
+    return res.status(200).json("recipe updated");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/api/fetchRecipes/", async (req, res) => {
+  try {
+    const {
+      id,
+      title,
+      image,
+      imagetype,
+      calories,
+      fat,
+      protein,
+      carbs,
+      diet,
+      readyin,
+    } = req.body;
+
+    if (
+      !(
+        id &&
+        title &&
+        image &&
+        imagetype &&
+        calories &&
+        fat &&
+        protein &&
+        carbs &&
+        diet &&
+        readyin
+      )
+    )
+      return res.json("provide all data");
+
+    await pool.query(
+      "INSERT into recipe (id, title, image, imagetype, calories,fat,protein,carbs,diet,readyin) VALUES ($1, $2, $3, $4, $5,$6,$7,$8,$9,$10)",
+      [
+        id,
+        title,
+        image,
+        imagetype,
+        calories,
+        fat,
+        protein,
+        carbs,
+        diet,
+        readyin,
+      ]
+    );
+
+    return res.status(200).json("recipe created");
+  } catch (err) {
+    res.status(400).json("something went wrong");
+    console.log(err);
+  }
+});
+
+app.delete("/api/fetchRecipes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM recipe WHERE id = $1", [id]);
+    return res.status(200).json("row deleted");
+  } catch (err) {
+    res.status(400).json("bad request");
     console.log(err);
   }
 });
